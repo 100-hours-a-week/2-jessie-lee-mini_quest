f5e4bb671789e9b1522d44b4425616fe7076ab81
jiyeeeah <sije3757@gmail.com>
2025-02-20 20:14:38 +0900

    자바스크립트 객체 - 2

---
+++ 01_JavascriptObject/02.js
@@ -0,0 +1,18 @@
+class Person {
+  #name;
+  #age;
+
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.#name} and I am ${this.#age} years old.`
+    );
+  }
+}
+
+const person = new Person("Jane Doe", 25);
+person.greet();
