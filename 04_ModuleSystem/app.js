c00ddc30df8fe1ddb377a189a5daa3497066cd27
jiyeeeah <sije3757@gmail.com>
2025-02-20 20:29:39 +0900

    모듈 시스템 - 2번 과제

--- 04_ModuleSystem/app.js
+++ 04_ModuleSystem/app.js
@@ -1,4 +1,16 @@
-const add = require("./math");
+// const add = require("./math");
 
-const sum = add(2, 3);
-console.log(sum);
+// const sum = add(2, 3);
+// console.log(sum);
+
+import { add, subtract } from "./operations.js";
+import User from "./userProfile.js";
+
+const sum2 = add(1, 2);
+console.log(sum2);
+
+const sub = subtract(3, 1);
+console.log(sub);
+
+const user = new User("Jessie", 28);
+user.greet();
---
+++ 04_ModuleSystem/userProfile.js
@@ -0,0 +1,15 @@
+export default class User {
+  #name;
+  #age;
+
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.#name} and I am ${this.#age} years old.`
+    );
+  }
+}
