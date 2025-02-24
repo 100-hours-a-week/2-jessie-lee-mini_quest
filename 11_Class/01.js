00343c07270906dfc4a48b676b8e972a730ed4b2
jiyeeeah <sije3757@snu.ac.kr>
2025-02-24 16:22:36 +0900

    클래스 - 1번 과제

---
+++ 11_Class/01.js
@@ -0,0 +1,18 @@
+class Rectangle {
+  width;
+  height;
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+
+  perimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
+
+export default Rectangle;
