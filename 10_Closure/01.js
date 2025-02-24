05ca6c4f74792af77520958af8523fc6e11a7844
jiyeeeah <sije3757@snu.ac.kr>
2025-02-24 14:47:40 +0900

    클로저 - 1번 과제

---
+++ 10_Closure/01.js
@@ -0,0 +1,12 @@
+function createCounter() {
+  let count = 0;
+  function increment() {
+    count++;
+  }
+  function decreament() {
+    count--;
+  }
+  function getCount() {
+    return count;
+  }
+}
